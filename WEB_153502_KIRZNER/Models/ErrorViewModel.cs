9b415b95ee38fcb134b690df5f1ca243b778d659
Nastya <kirznernasta@gmail.com>
2023-08-25 01:46:36 +0300

    add lab 1

---
+++ WEB_153502_KIRZNER/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB_153502_KIRZNER.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
